ff151314cd142beba42158159e312fc8e1e84d86
sydnash <157621271@qq.com>
2017-06-27 14:17:09 +0800

    add filename liennumber funcname to log.

--- log/log.go
+++ log/log.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"sync"
+	"runtime"
 )
 
 // levels
@@ -39,6 +40,8 @@ type Logger interface {
 var glogger Logger
 var gloggerMut sync.Mutex
 
+var HasCallerPos bool
+
 func do(level int, desc, format string, param ...interface{}) {
 	if glogger == nil {
 		log.Fatal("log is not init, please call log.Init first.")
@@ -59,23 +62,42 @@ func SetLogger(logger Logger) {
 	glogger = logger
 	gloggerMut.Unlock()
 }
+
+func preProcess(format string) string {
+	if !HasCallerPos {
+		return format
+	}
+	pc, file, line, ok := runtime.Caller(2)
+	if ok {
+		t := runtime.FuncForPC(pc)
+		name := t.Name()
+		format = fmt.Sprintf("[%v:%v +%v]       ", file, line, name) + format
+	}
+	return format
+}
+
 func Debug(format string, param ...interface{}) {
+	format = preProcess(format)
 	do(DEBUG_LEVEL, DEBUG_LEVEL_DESC, format, param...)
 }
 
 func Info(format string, param ...interface{}) {
+	format = preProcess(format)
 	do(INFO_LEVEL, INFO_LEVEL_DESC, format, param...)
 }
 
 func Warn(format string, param ...interface{}) {
+	format = preProcess(format)
 	do(WARN_LEVEL, WARN_LEVEL_DESC, format, param...)
 }
 
 func Error(format string, param ...interface{}) {
+	format = preProcess(format)
 	do(ERROR_LEVEL, ERROR_LEVEL_DESC, format, param...)
 }
 
 func Fatal(format string, param ...interface{}) {
+	format = preProcess(format)
 	do(FATAL_LEVEL, FATAL_LEVEL_DESC, format, param...)
 }
 
@@ -88,5 +110,6 @@ func Init(path string, fileLevel, shellLevel, maxLine, bufSize int) Logger {
 
 //init log with default logger
 func init() {
+	HasCallerPos = true
 	SetLogger(DefaultLogger)
 }
